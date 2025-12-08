20592e4371d2be7b337348093264d59fdb80cae2
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2025-12-08 09:56:57 +0530

    Time: 1 ms (99.60%) | Memory: 47 MB (52.19%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int helper(int[][] arr,int i,int j,int[][] dp){
+        int n=arr.length;
+        if(j<0 || j>=n) return Integer.MAX_VALUE;
+        if(i==n-1){
+            return arr[i][j];
+        }
+        if(dp[i][j]!=Integer.MAX_VALUE){
+            return dp[i][j];
+        }
+        int s1=helper(arr,i+1,j-1,dp);
+        int s2=helper(arr,i+1,j,dp);
+        int s3=helper(arr,i+1,j+1,dp);
+        dp[i][j]=arr[i][j]+Math.min(s1,Math.min(s2,s3));
+        return dp[i][j];
+    }
+    public int minFallingPathSum(int[][] arr) {
+        int n=arr.length;
+        int[][] dp=new int[n+1][n+1];
+        for(int i=0;i<n+1;i++){
+            Arrays.fill(dp[i],Integer.MAX_VALUE);
+        }
+        int minSum=Integer.MAX_VALUE;
+        for(int j=0;j<n;j++){
+            minSum=Math.min(minSum,helper(arr,0,j,dp));
+        }
+        return minSum;
+    }
+}
