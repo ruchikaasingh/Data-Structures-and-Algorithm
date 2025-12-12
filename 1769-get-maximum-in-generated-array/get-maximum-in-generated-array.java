c6ab77f5df6580aaf2066077bd114946354c57af
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2025-12-12 23:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (44.47%) - LeetSync

---
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if (n == 0) return 0;            // handle n = 0
+        int[] nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        int max = 1;
+
+        for (int i = 2; i <= n; i++) {  // iterate only up to n
+            if (i % 2 == 0) {
+                nums[i] = nums[i / 2];
+            } else {
+                nums[i] = nums[i / 2] + nums[i / 2 + 1];
+            }
+            if (nums[i] > max) max = nums[i];
+        }
+        return max;
+    }
+}
