0b8ce2031c03edcabfe6f486e73a9f29f9df4218
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2025-12-16 14:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (71.41%) - LeetSync

---
+++ 174-dungeon-game/dungeon-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    int m, n;
+    int[][] memo;
+
+    public int calculateMinimumHP(int[][] dungeon) {
+        m = dungeon.length;
+        n = dungeon[0].length;
+        memo = new int[m][n];
+        return fxn(0, 0, dungeon);
+    }
+
+    int fxn(int i, int j, int[][] dungeon) {
+        // out of bounds
+        if (i >= m || j >= n) return Integer.MAX_VALUE;
+
+        // princess cell (bottom-right)
+        if (i == m - 1 && j == n - 1) {
+            return Math.max(1, 1 - dungeon[i][j]);
+        }
+
+        if (memo[i][j] != 0) return memo[i][j];
+
+        int right = fxn(i, j + 1, dungeon);
+        int down  = fxn(i + 1, j, dungeon);
+
+        int need = Math.min(right, down) - dungeon[i][j];
+        memo[i][j] = Math.max(1, need);
+
+        return memo[i][j];
+    }
+}
