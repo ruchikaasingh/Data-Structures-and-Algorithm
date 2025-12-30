7a81138dd1364888f84193ba50caaa23e91c2b3f
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2025-12-30 13:51:28 +0530

    Time: 4 ms (52.01%) | Memory: 44.7 MB (49.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,10 +1,18 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        String clean = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int left = 0, right = clean.length() - 1; 
+    public boolean isPalindrome(String s) 
+    {
+        //filter the string using sb functions
+        StringBuilder sb= new StringBuilder();
+        for(char c: s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        int left = 0, right = sb.length() - 1; 
         
         while (left < right) {                     
-            if (clean.charAt(left++) != clean.charAt(right--)) return false;
+            if (sb.charAt(left++) != sb.charAt(right--)) return false;
         }
         return true;
     }
