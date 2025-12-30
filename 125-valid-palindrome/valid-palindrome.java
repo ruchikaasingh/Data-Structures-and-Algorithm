db80229aefd58bef90fe966fe2422d13beac389d
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2025-12-30 13:40:22 +0530

    Time: 14 ms (32.52%) | Memory: 46.5 MB (23.64%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.length()==1) return true;
-        String clean= s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String clean = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0, right = clean.length() - 1; 
         
-        int left=0;
-        int right= clean.length()-1;
-        while(left<right){
-            if (clean.charAt(left)== clean.charAt(right)){
-                left++;
-                right--;
-            }
-            else return false;
+        while (left < right) {                     
+            if (clean.charAt(left++) != clean.charAt(right--)) return false;
         }
         return true;
     }
