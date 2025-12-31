a62862e8c80b756587f4a26e8731d58940f62a83
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2025-12-31 16:03:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (15.00%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        int n= nums.length;
+        int n = nums.length;
 
-        int dp[]= new int[n];
-        Arrays.fill(dp, -1);
+        if (n == 1) return nums[0];
 
-        return fxn(0, nums, dp);
-    }
+        int[] dp = new int[n];
 
-    public int fxn(int i, int nums[], int dp[]){
-        if(i>=nums.length) return 0;
-        if(dp[i]!= -1) return dp[i];
+        // Base cases
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        int inc=0;
-        inc = nums[i]+ fxn(i+2, nums, dp);
+        // Build table
+        for (int i = 2; i < n; i++) {
+            int inc = nums[i] + dp[i - 2];
+            int exc = dp[i - 1];
+            dp[i] = Math.max(inc, exc);
+        }
 
-        int exc= fxn(i+1, nums, dp);
-
-        return dp[i]= Math.max(inc, exc);
+        return dp[n - 1];
     }
 }
