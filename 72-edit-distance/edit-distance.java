8f8b5d7d6ef4ad83056c3508fbd5f6779f52c81e
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2026-01-17 21:28:32 +0530

    Time: 3 ms (96.67%) | Memory: 47.1 MB (75.80%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,26 +1,33 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
-
-        // Base cases
-        for (int i = 0; i <= m; i++) dp[i][0] = i;
-        for (int j = 0; j <= n; j++) dp[0][j] = j;
-
-        // Fill
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1]; // no change
-                } else {
-                    dp[i][j] = 1 + Math.min(
-                        dp[i - 1][j - 1], // replace
-                        Math.min(dp[i - 1][j], // delete
-                                 dp[i][j - 1]) // insert
-                    );
-                }
+        int[][] memo = new int[m + 1][n + 1];
+        
+        // Initialize with -1 meaning its not computed yet
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                memo[i][j] = -1;
             }
         }
-        return dp[m][n];
+        
+        return fxn(m, n, word1, word2, memo);
+    }
+    
+    private int fxn(int i, int j, String word1, String word2, int[][] memo) {
+        if (i == 0) return j; // need j inserts
+        if (j == 0) return i; // need i deletes
+        
+        if (memo[i][j] != -1) return memo[i][j];
+        
+        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+            memo[i][j] = fxn(i - 1, j - 1, word1, word2, memo);
+        } else {
+            int insert = fxn(i, j - 1, word1, word2, memo);
+            int delete = fxn(i - 1, j, word1, word2, memo);
+            int replace = fxn(i - 1, j - 1, word1, word2, memo);
+            memo[i][j] = 1 + Math.min(replace, Math.min(insert, delete));
+        }
+        
+        return memo[i][j];
     }
 }
