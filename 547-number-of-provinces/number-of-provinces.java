cf548d00934c963332dc2fdaf2e76a4b8f38a5d9
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2026-02-23 22:29:51 +0530

    Time: 2 ms (47.79%) | Memory: 47.3 MB (75.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,22 +1,47 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        boolean[] visited= new boolean[n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                count++;
-                dfs(i,isConnected,visited);
+    static int parent[], size[];
+    static int component;
+    
+    public static int find(int x){
+        if(parent[x]==x)return x;
+        return parent[x]= find(parent[x]);
+    }
+    
+    public static void union(int u, int v){
+        int pu= find(u);
+        int pv= find(v);
+        
+        if(pu==pv) return;
+        if(size[pu]< size[pv]){
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }
+        else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+        component--;
+    }
+    public int findCircleNum(int[][] isconnected) {
+        int v= isconnected.length;
+        parent= new int[v];
+        size= new int [v];
+        component= v;
+        
+        //initialize dsu
+        for(int i=0; i<v; i++){
+            size[i]=1;
+            parent[i]=i;
+        }
+
+        for(int i=0; i<v; i++){
+            for(int j=0; j<v; j++){
+                if(i!=j && isconnected[i][j]==1){
+                    union(i,j);
+                }
             }
         }
-        return count;
-    }
-    private void dfs(int city, int[][] isConnected,boolean[] visited){
-        visited[city]=true;
-        for(int n=0;n<isConnected.length;n++){
-            if(isConnected[city][n]==1 && !visited[n]){
-                dfs(n,isConnected,visited);
-            }
-        }
-    }
+        return component;
+
+    }   
 }
