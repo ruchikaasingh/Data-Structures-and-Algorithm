55f3088e5154e8e6359fa689a627acedb792b0a8
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2026-03-05 00:35:09 +0530

    Time: 2 ms (94.84%) | Memory: 47.3 MB (26.69%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,39 +1,29 @@
 class Solution {
-    int m, n;
-
     public int numSpecial(int[][] mat) {
-        m = mat.length;
-        n = mat[0].length;
-        int ans = 0;
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] rowSum = new int[m];
+        int[] colSum = new int[n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 1) {
-                    if (dfs(i, j, mat)) {
-                        ans++;
-                    }
+                    rowSum[i]++;
+                    colSum[j]++;
                 }
             }
         }
+
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1) {
+                    ans++;
+                }
+            }
+        }
+
         return ans;
-    }
-
-    public boolean dfs(int i, int j, int[][] mat) {
-        int[] dr = {0, 0, -1, 1};
-        int[] dc = {-1, 1, 0, 0};
-
-        for (int d = 0; d < 4; d++) {
-            int nr = i + dr[d];
-            int nc = j + dc[d];
-
-            while (nr >= 0 && nr < m && nc >= 0 && nc < n) {
-                if (mat[nr][nc] == 1 && !(nr == i && nc == j)) {
-                    return false; 
-                }
-                nr += dr[d];
-                nc += dc[d];
-            }
-        }
-        return true;
     }
 }
