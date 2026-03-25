d982ed49b357d4c8d7071262d257190781f1c551
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2026-03-25 10:46:37 +0530

    Time: 50 ms (9.70%) | Memory: 101.3 MB (5.01%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,25 +1,64 @@
+import java.util.*;
+
 class Solution {
+    class Dsu {
+        int[] parent, size;
+
+        public Dsu(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] == x) return x;
+            return parent[x] = find(parent[x]); // path compression
+        }
+
+        public void union(int u, int v) {
+            int pu = find(u);
+            int pv = find(v);
+            if (pu == pv) return;
+
+            if (size[pu] < size[pv]) {
+                parent[pu] = pv;
+                size[pv] += size[pu];
+            } else {
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+        }
+    }
+
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
 
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) set.add(num);
+        //map number, idx
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        Dsu dsu = new Dsu(n);
 
-        int longest = 0;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) continue; // avoid duplicate
+            map.put(nums[i], i);
 
-        for (int num : set) {
-            // only start if its the beginning of a sequence
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int streak = 1;
-
-                while (set.contains(currentNum + 1)) {
-                    currentNum++;
-                    streak++;
-                }
-                longest = Math.max(longest, streak);
+            // Union with neighbors if they exist
+            if (map.containsKey(nums[i] - 1)) {
+                dsu.union(i, map.get(nums[i] - 1));
+            }
+            if (map.containsKey(nums[i] + 1)) {
+                dsu.union(i, map.get(nums[i] + 1));
             }
         }
-        return longest;
+
+        //find largest component size
+        int maxLen = 1;
+        for (int i = 0; i < n; i++) {
+            maxLen = Math.max(maxLen, dsu.size[dsu.find(i)]);
+        }
+        return maxLen;
     }
 }
