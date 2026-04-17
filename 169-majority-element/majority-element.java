1e7d28b70aa66f34d6f3824c91ac9ea4475df9d3
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2026-04-17 13:34:39 +0530

    Time: 1 ms (99.84%) | Memory: 55.8 MB (36.72%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n= nums.length;
+        int count =0;
+        int candidate=0;
 
-        HashMap<Integer, Integer> map= new HashMap<>();   //stores number and its freq
-        for(int i=0; i<n; i++){
-            int count= map.getOrDefault(nums[i], 0)+1;
-            map.put(nums[i], count);
-
-            if(count> n/2){
-                return nums[i];
+        for(int elem: nums){
+            if(count == 0){
+                candidate= elem;
             }
+
+            if(elem == candidate){
+                count++;
+            }
+            else count--;
         }
-        return 0;
+        return  candidate;
     }
 }
