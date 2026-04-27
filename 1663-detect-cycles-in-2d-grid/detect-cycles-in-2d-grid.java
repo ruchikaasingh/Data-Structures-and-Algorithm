dfe6e8b23cd94702e24461ecbdcdf5290706f5de
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2026-04-27 15:11:04 +0530

    Time: 18 ms (80.04%) | Memory: 138.4 MB (63.40%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -5,17 +5,17 @@ class Solution {
     public boolean containsCycle(char[][] grid) {
         n = grid.length;
         m = grid[0].length;
-        boolean[] visit = new boolean[n*m];
+        boolean[][] visit = new boolean[n][m];
 
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                if (!visit[i * m + j] && dfs(i, j, -1, -1, grid, visit))
+                if (!visit[i][j] && dfs(i, j, -1, -1, grid, visit))
                     return true;
         return false;
     }
 
-    private static boolean dfs(int r, int c, int pr, int pc, char[][] grid, boolean[] visit) {
-        visit[r * m + c] = true;
+    private static boolean dfs(int r, int c, int pr, int pc, char[][] grid, boolean[][]visit) {
+        visit[r][c] = true;
         
         for (int[] d : dirs) {
             int nr = r + d[0];
@@ -23,7 +23,7 @@ class Solution {
             if (nr != pr || nc != pc)
                 if (nr >= 0 && nr < n && nc >= 0 && nc < m)
                     if (grid[nr][nc] == grid[r][c])
-                        if (visit[nr * m + nc] || dfs(nr, nc, r, c, grid, visit))
+                        if (visit[nr][nc] || dfs(nr, nc, r, c, grid, visit))
                             return true;
         }
         return false;
