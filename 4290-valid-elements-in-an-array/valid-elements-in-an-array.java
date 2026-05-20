b77877212f19e927083d27280de91b2c0f2e9144
Ruchika Singh <167904140+ruchikaasingh@users.noreply.github.com>
2026-05-20 19:16:13 +0530

    Time: 2 ms (21.21%) | Memory: 46.6 MB (77.27%) - LeetSync

---
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findValidElements(int[] nums) {
+        int n = nums.length;
+
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        //first and last elements are always valid
+        left[0] = 1;
+        right[n - 1] = 1;
+
+        //left elems
+        for (int i = 1; i < n; i++) {
+            boolean isGreater = true;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] <= nums[j]) {
+                    isGreater = false;
+                    break;
+                }
+            }
+            if (isGreater) left[i] = 1;
+        }
+
+        // right elems
+        for (int i = 0; i < n - 1; i++) {
+            boolean isGreater = true;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] <= nums[j]) {
+                    isGreater = false;
+                    break;
+                }
+            }
+            if (isGreater) right[i] = 1;
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (left[i] == 1 || right[i] == 1) {
+                ans.add(nums[i]);
+            }
+        }
+
+        return ans;
+    }
+}
